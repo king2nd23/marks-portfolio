c42bf0fa9a7e20a4293ee2b268d3cdc915e98aa7
Mark King <king2nd23@gmail.com>
2019-07-20 15:58:18 -0400

    Updates

---
+++ precache-manifest.4a28fe26aa52d49f594bae8b086cd3d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ae0e79de1d88ddf265affabe315930",
+    "url": "/marks-portfolio/index.html"
+  },
+  {
+    "revision": "0b0bc7f2404a708e9588",
+    "url": "/marks-portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/marks-portfolio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "0b0bc7f2404a708e9588",
+    "url": "/marks-portfolio/static/js/main.a9eb4160.chunk.js"
+  },
+  {
+    "revision": "dcfee81d56c9792a3d5a",
+    "url": "/marks-portfolio/static/js/runtime~main.9f5fee43.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/marks-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
