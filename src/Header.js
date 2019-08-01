2f3818a14ee1b5df9f76e30a8c0a79ec2593aba3
Mark King <king2nd23@gmail.com>
2019-07-31 21:22:59 -0400

    using font-awesome icons for nav

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
 import logo from "./favicon.ico";
-import mainCSS from "./main.css"
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import mainCSS from "./main.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAddressBook, faFileCode, faFileAlt, faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 
 //need to add on hover to the entire nav menu that adds color to the icons
 //need to add 2nd hover to individual nav icons that adds a light gradient
@@ -12,12 +13,22 @@ const Header = () => {
   return(
     <header>
       <div className="navBar">
-        <div class="logo flexItem"><img src={logo} alt="mk logo"/></div>
-        <div className="aboutLink flexItem">about</div>
-        <div className="portfolioLink flexItem">portfolio</div>
-        <div className="resumeLink flexItem">resume</div>
-        <div className="emailLink flexItem"><FontAwesomeIcon icon={faEnvelope} /></div>
-        <div className="gitLink flexItem"><FontAwesomeIcon icon={faGithub} /></div>
+        <div class="logo flexItem"></div>
+        <div className="aboutLink flexItem"><FontAwesomeIcon icon={faAddressBook} size="6x"/>
+          <span>about</span>
+        </div>
+        <div className="aboutLink flexItem"><FontAwesomeIcon icon={faFileCode} size="6x"/>
+          <span>portfolio</span>
+        </div>
+        <div className="aboutLink flexItem"><FontAwesomeIcon icon={faFileAlt} size="6x"/>
+          <span>resume</span>
+        </div>
+        <div className="aboutLink flexItem"><FontAwesomeIcon icon={faGithub} size="6x"/>
+          <span>github</span>
+        </div>
+        <div className="aboutLink flexItem"><FontAwesomeIcon icon={faEnvelope} size="6x"/>
+          <span>email</span>
+        </div>
       </div>
     </header>
   )
