888159e80f6565bfa3f0caa40f3c625c62db90ec
Mark King <king2nd23@gmail.com>
2019-08-07 20:59:01 -0400

    added some content to professional summary

--- src/About.js
+++ src/About.js
@@ -2,14 +2,22 @@ import React from 'react';
 import mainCSS from "./main.css";
 
 const About = () => {
+  const intro = "Hi. I'm Mark, a Web Developer located in the Raleigh-Durham area. ";
+  const schoolInfo = "Studying Information Sciences & Technology at Penn State University";
+  const mySummary = "Skilled at writing efficient, well-designed, and testable code\
+   using current best practices. Capable of managing web page creation and repair\
+    projects with full accuracy and efficiency.";
+
+
   return(
     <div className="aboutPage">
       <div className="introSection">
-        <div className="salutation">
-          <h2>Hello</h2>
+        <div className="introduction">
+          <h1>{intro}</h1>
+          <p>{schoolInfo}</p>
         </div>
         <div className="summary">
-          <p>My name is Mark King...</p>
+          <p>{mySummary}</p>
         </div>
       </div>
     </div>
